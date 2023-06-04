b5f62d8fd7a5f13867df11a2dffdfeb70038768b
wlsdbdh <129045769+wlsdbdh@users.noreply.github.com>
2023-06-04 16:24:17 +0900

    Add files via upload

---
+++ w13/master.js
@@ -0,0 +1,13 @@
+function change_btn(e) {
+  var btns = document.querySelectorAll(".button");
+  btns.forEach(function (btn, i) {
+    if (e.currentTarget == btn) {
+      btn.classList.add("active");
+    } else {
+      btn.classList.remove("active");
+    }
+  });
+  console.log(e.currentTarget);
+}
+  
+  
